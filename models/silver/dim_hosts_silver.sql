b68f21c3a07d5c2987473ab7bb9a8c6ad714dfc5
Edgar Avilan <edgaravilanfr@gmail.com>
2025-07-07 16:35:02 +0000

    check

---
+++ models/silver/dim_hosts_silver.sql
@@ -0,0 +1,9 @@
+with hosts as (select * from {{ ref("hosts_raw") }})
+
+select
+    host_id,
+    coalesce(host_name, 'Anonymous') as host_name,
+    is_superhost,
+    host_created_at,
+    host_updated_at
+from hosts
---
+++ models/silver/dim_listings_silver.sql
@@ -0,0 +1,14 @@
+with listings as (
+    select * from {{ref("listings_raw")}}
+)
+
+select listing_id,
+listing_name,
+room_type,
+case when minimum_nights = 0 then 1
+    else minimum_nights end as minimum_nights,
+host_id,
+created_at,
+updated_at,
+SAFE_CAST(price_str as FLOAT64) as price
+from listings
